321e53bb8f22e73d2f11c205381c7a59189d2599
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-04-30 14:56:50 +0300

    Created project

---
+++ app/src/main/java/com/atakanmadanoglu/nasa_rover_vehicles/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.atakanmadanoglu.nasa_rover_vehicles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
