98fa51cd743c2124f6f7152a26d1a81f4781d3c5
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-05-01 16:23:53 +0300

    Designed CuriosityPhotosFragment

--- app/src/main/java/com/atakanmadanoglu/nasa_rover_vehicles/view/CuriosityPhotosFragment.kt
+++ app/src/main/java/com/atakanmadanoglu/nasa_rover_vehicles/view/CuriosityPhotosFragment.kt
@@ -5,56 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.atakanmadanoglu.nasa_rover_vehicles.R
+import com.atakanmadanoglu.nasa_rover_vehicles.databinding.FragmentCuriosityPhotosBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CuriosityPhotosFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CuriosityPhotosFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentCuriosityPhotosBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_curiosity_photos, container, false)
+        _binding = FragmentCuriosityPhotosBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CuriosityPhotosFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CuriosityPhotosFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onResume() {
+        super.onResume()
+        val filterOptions = resources.getStringArray(R.array.curiosityFilterOptions)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, filterOptions)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
     }
+
 }
