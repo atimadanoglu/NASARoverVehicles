11ec2f31087bea0a8f152bf372d83b045e66223a
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-05-04 01:29:13 +0300

    Set up viewPager and tabLayout

--- app/src/main/java/com/atakanmadanoglu/nasa_rover_vehicles/view/CuriosityPhotosFragment.kt
+++ app/src/main/java/com/atakanmadanoglu/nasa_rover_vehicles/view/CuriosityPhotosFragment.kt
@@ -1,12 +1,10 @@
 package com.atakanmadanoglu.nasa_rover_vehicles.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import com.atakanmadanoglu.nasa_rover_vehicles.R
+import androidx.fragment.app.Fragment
 import com.atakanmadanoglu.nasa_rover_vehicles.databinding.FragmentCuriosityPhotosBinding
 
 class CuriosityPhotosFragment : Fragment() {
@@ -20,15 +18,7 @@ class CuriosityPhotosFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentCuriosityPhotosBinding.inflate(inflater, container, false)
-
         return binding.root
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val filterOptions = resources.getStringArray(R.array.curiosityFilterOptions)
-        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, filterOptions)
-        binding.autoCompleteTextView.setAdapter(arrayAdapter)
     }
 
 }
--- app/src/main/java/com/atakanmadanoglu/nasa_rover_vehicles/view/MainActivity.kt
+++ app/src/main/java/com/atakanmadanoglu/nasa_rover_vehicles/view/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.atakanmadanoglu.nasa_rover_vehicles.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.atakanmadanoglu.nasa_rover_vehicles.R
+import com.atakanmadanoglu.nasa_rover_vehicles.databinding.ActivityMainBinding
+import com.atakanmadanoglu.nasa_rover_vehicles.view.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabLayout
+
+        val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = adapter
+
+        val names = resources.getStringArray(R.array.tabNames)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = names[position]
+        }.attach()
     }
 }
